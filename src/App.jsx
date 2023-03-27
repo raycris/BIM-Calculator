d1fc3acac5c841e80c435db6cea38a480365268b
Raycris Maldonado <raycris.maldonado@utreee.com>
2023-03-26 20:28:12 -0400

    adding memo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useMemo(() => first, [second])
 
   return (
     <div >
